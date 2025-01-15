1d7236b2c2c6773ae1340b80d4a373ad20da2bf8
dylanbarnard <dylan.barnard@standardbank.co.za>
2025-01-15 19:40:33 +0200

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    assetModuleFilename: 'images/[name][hash][ext][query]',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "images/[name][hash][ext][query]",
     clean: true,
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:6200',
+      "/api": {
+        target: "http://localhost:6200",
         changeOrigin: true,
       },
     },
     historyApiFallback: true,
-    static: path.resolve(__dirname, 'src'),
+    static: path.resolve(__dirname, "src"),
     port: 3000,
     open: true,
     headers: {
-      'Content-Security-Policy': "default-src 'self'; connect-src 'self' http://localhost:6200;",
+      "Content-Security-Policy":
+        "default-src 'self'; connect-src 'self' http://localhost:6200;",
     },
   },
   module: {
@@ -35,27 +36,30 @@ export default {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'images/[name][hash][ext][query]',
+          filename: "images/[name][hash][ext][query]",
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body',
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+      templateParameters: {
+        appContainer: '<div id="app"></div>',
+      },
     }),
   ],
 };
